064ef392099f120223e4fb900e25ff1e0038b46a
luismiguelbravo <lmb_gott@hotmail.com>
2016-11-16 15:44:20 -0300

    se agregan los dias desde que se solicito la visa de trabajo

--- dias.js
+++ dias.js
@@ -2,6 +2,7 @@
  2016 Luis Miguel Bravo
 */
 var fecha_llegada_a_chile = new Date("10/19/2016");
+var fecha_envio_correo_visa = new Date("10/25/2016");
 var fecha_mudanza_nuevo_departamento = new Date("5/1/2017");
 var fecha_maraton_de_programacion = new Date("11/26/2016");
 var fecha_de_inicio_2brains = new Date("11/2/2016");
@@ -11,6 +12,7 @@ var fecha_de_new_year = new Date("12/31/2016");
 
 var hoy = new Date(Dia_de_hoy());
 document.write("dias en chile :" + Diferencia_En_Dias(fecha_llegada_a_chile, hoy) + "<br />");
+document.write("dias desde que solicite la visa de trabajo :" + Diferencia_En_Dias(fecha_envio_correo_visa, hoy) + "<br />");
 document.write("dias desde que inicie en 2BRAINS :" + Diferencia_En_Dias(hoy, fecha_de_inicio_2brains) + "<br />");
 document.write("dias para el maraton de programación :" + Diferencia_En_Dias(hoy, fecha_maraton_de_programacion) + "<br />");
 document.write("dias para mudarme :" + Diferencia_En_Dias(hoy, fecha_mudanza_nuevo_departamento) + "<br />");
